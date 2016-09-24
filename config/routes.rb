efb78750e1bb629425b895a8acc20cd082188c3f
Tomas Maly <tmaly1980@gmail.com>
2016-09-24 12:19:56 -0400

    Saving

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Multisite::Engine.routes.draw do
-	get '/site_designs/theme(/:theme).css', to: 'site_designs#theme', as: 'theme_css'
+	get '/site_designs/theme(/:theme)', to: 'site_designs#theme', as: 'theme_css'
 
 	resources :sites, :site_design, :sidebar
 
